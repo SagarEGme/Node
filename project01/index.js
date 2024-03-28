5eb38e7e8398ad62cdef23ff3bb7c44924953f50
SagarEGme <sagaregmi0710@gmail.com>
2024-03-28 19:02:00 +0545

    project 1 cont.

--- project01/index.js
+++ project01/index.js
@@ -1,6 +1,50 @@
 const express = require("express");
 const data = require("./MOCK_DATA.json")
 const app = express();
+const fs = require("fs")
+
+
+// app.get("/api/users/:uid",(req,res)=>{
+//     const id = Number(req.params.uid);
+//     const user = data.find((user)=>user.id === id)
+    
+//     res.json(user)
+// })
+
+// if we have to do work on the same route again and again
+// then we can use this method:
+app.route("/api/users/:uid").get((req,res)=>{
+    const id = Number(req.params.uid);
+    const user = data.find((user)=>user.id === id)
+    res.json(user)
+}).patch((req,res)=>{
+    // edit user with id
+    return res.json({status : "pending"})
+}).post((req,res)=>{
+    // post user with id
+    return res.json({status : "pending"})
+}).delete((req,res)=>{
+    // delete user with id
+    return res.json({status : "pending"})
+})
+
+// To receive data from frontend, use middleware(plugins)
+app.use(express.urlencoded({extended : false}))
+
+// we can create our own middlewares 
+app.use((req,res,next)=>{
+    // It will hold the response and it wont send response:userdata in this case
+    // console.log("hello from middleware1")
+    // return res.send("hello world");
+
+    req.myName = "sagar" //This will now be accessible from anywhere
+    next();
+})
+app.use((req,res,next)=>{
+    fs.appendFile("logUser.txt",`\n ${Date.now()} : ${req.method} : ${req.path}`,(err,data)=>{
+        next();
+    })
+})
 
 // for mobile user -> They require data rather than JSON file which can undergo SSR/CSR in browsers
 app.get("/users", (req,res)=>{
@@ -17,29 +61,14 @@ app.get("/api/users",(req,res)=>{
     return res.json(data)
 })
 
-// app.get("/api/users/:uid",(req,res)=>{
-//     const id = Number(req.params.uid);
-//     const user = data.find((user)=>user.id === id)
-    
-//     res.json(user)
-// })
-// if we have to do work on the same route again and again
-// then we can use this method:
+app.post("/api/users",(req,res)=>{
+    const body= req.body;
+    data.push({id:data.length+1 ,...body})
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(data),(err,data)=>{
 
-app.route("/api/users/:uid").get((req,res)=>{
-    const id = Number(req.params.uid);
-    const user = data.find((user)=>user.id === id)
-    res.json(user)
-}).patch((req,res)=>{
-    // edit user with id
-    return res.json({status : "pending"})
-}).post((req,res)=>{
-    // post user with id
-    return res.json({status : "pending"})
-}).delete((req,res)=>{
-    // delete user with id
-    return res.json({status : "pending"})
+        return res.json({status:"pending"})
+    })
 })
 const PORT = 8000;
 
-app.listen(PORT,()=>console.log("server started"))  
+app.listen(PORT,()=>console.log("server started at " + PORT))  
